d581d6aaeb6e6ad240caf2e60104ffae37ae73f7
KrijnSlot <krijnslot112233@gmail.com>
2025-05-13 12:01:06 +0200

    Moving Platforms

--- Assets/Script/Simple2DMovScript.cs
+++ Assets/Script/Simple2DMovScript.cs
@@ -13,60 +13,108 @@ public class Movement : MonoBehaviour
     private SpriteRenderer sr;
     private Animator animator;
 
-    public bool isGrounded; 
-    public GameObject groundCheckPoint;
+    public bool isGrounded;
+    public Transform groundCheckPoint;
     public float groundCheckRadius;
     public LayerMask groundLayer;
 
     private bool jumpPressed = false;
-    private bool APressed = false; 
-    private bool DPressed = false; 
+    private bool APressed = false;
+    private bool DPressed = false;
+
+    private Transform currentPlatform = null;
+    private Vector3 lastPlatformPos;
 
     void Awake()
     {
-        body = GetComponent<Rigidbody2D>(); 
+        body = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
-        animator = GetComponent<Animator>(); 
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) jumpPressed = true; 
-        if (Input.GetKey(KeyCode.A)) APressed = true; 
-        if (Input.GetKey(KeyCode.D)) DPressed = true; 
+        if (Input.GetKeyDown(KeyCode.Space)) jumpPressed = true;
+        if (Input.GetKey(KeyCode.A)) APressed = true;
+        if (Input.GetKey(KeyCode.D)) DPressed = true;
     }
 
     void FixedUpdate()
     {
-        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.transform.position, groundCheckRadius, groundLayer);
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundLayer);
 
+        // Player input movement
         if (APressed)
         {
-            body.velocity = new Vector2(-runSpeed, body.velocity.y); 
-            transform.eulerAngles = new Vector3(transform.eulerAngles.x, 180, transform.eulerAngles.z);
-            APressed = false; 
+            body.velocity = new Vector2(-runSpeed, body.velocity.y);
+            transform.eulerAngles = new Vector3(0, 180, 0);
+            APressed = false;
         }
         else if (DPressed)
         {
             body.velocity = new Vector2(runSpeed, body.velocity.y);
-            transform.eulerAngles = new Vector3(transform.eulerAngles.x, 0, transform.eulerAngles.z); 
-            DPressed = false; 
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            DPressed = false;
         }
-        else body.velocity = new Vector2(0, body.velocity.y);
+        else
+        {
+            body.velocity = new Vector2(0, body.velocity.y);
+        }
 
-
+        // Jump
         if (jumpPressed && isGrounded)
         {
-            body.velocity = new Vector2(0, jumpForce);
-            jumpPressed = false; 
+            body.velocity = new Vector2(body.velocity.x, jumpForce);
+            jumpPressed = false;
+            currentPlatform = null;
         }
 
- 
+        // Platform follow logic
+        if (currentPlatform != null)
+        {
+            Vector3 delta = currentPlatform.position - lastPlatformPos;
+            transform.position += delta;
+            lastPlatformPos = currentPlatform.position;
+        }
+
+        // Animation
         if (!isGrounded)
         {
-            animator.enabled = false; 
+            animator.enabled = false;
             sr.sprite = jumpSprite;
         }
-        else animator.enabled = true; 
+        else
+        {
+            animator.enabled = true;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("MovingPlatform"))
+        {
+            foreach (ContactPoint2D contact in collision.contacts)
+            {
+                if (contact.normal.y > 0.5f) 
+                {
+                    currentPlatform = collision.transform;
+                    lastPlatformPos = currentPlatform.position;
+                    Debug.Log("Landed on moving platform");
+                    break;
+                }
+            }
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("MovingPlatform"))
+        {
+            if (currentPlatform == collision.transform)
+            {
+                currentPlatform = null;
+                Debug.Log("Left moving platform");
+            }
+        }
     }
 }
