e5babf318d027d2efe6e6a8643557899912573a0
KrijnSlot <krijnslot112233@gmail.com>
2025-05-14 11:08:19 +0200

    moving platforms

--- Assets/Ryan Prototype/Scripts/Player/PlayerMovemenet.cs
+++ Assets/Ryan Prototype/Scripts/Player/PlayerMovemenet.cs
@@ -1,68 +1,144 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMovemenet : MonoBehaviour
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(PlayerInput))]
+public class PlayerMovement : MonoBehaviour
 {
-    public float speed;
-    [SerializeField] float jumpStrenght = 400;
-    private Vector2 moveInput;
-    Rigidbody2D rb;
+    [Header("Movement")]
+    public float runSpeed = 6f;
+
+    [Header("Jumping")]
+    public float jumpForce = 8f;
+    public float coyoteTime = 0.2f;
+    private float coyoteTimer = -1;
+    public bool canJump = false;
+
+    [Header("Ground Check")]
+    public Transform groundCheckPoint;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+
+    [Header("Platform Following")]
+    private Transform currentPlatform = null;
+    private Vector3 lastPlatformPos;
+
+    [Header("Graphics")]
+    public Sprite jumpSprite;
+    private SpriteRenderer sr;
+    private Animator animator;
+
+    private Rigidbody2D rb;
     private PlayerInput playerInput;
-    private InputAction moveAction;
-    [SerializeField] bool canJump = false;
-    [SerializeField] float coyoteTime;
-    [SerializeField] float coyoteTimer = -1;
-
-
+    private Vector2 moveInput;
+    private bool jumpPressed;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
         playerInput = GetComponent<PlayerInput>();
+
+        playerInput.actions["Jump"].performed += Jump;
+    }
+
+    private void Update()
+    {
+        moveInput = playerInput.actions["Move"].ReadValue<Vector2>();
+
+        bool isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundLayer);
+
+        if (rb.velocity.y < 0 && isGrounded)
+        {
+            canJump = true;
+            Debug.Log("canJump");
+            coyoteTimer = -1;
+        }
+        else
+        {
+            if (coyoteTimer <= 0)
+                coyoteTimer = coyoteTime;
+        }
+
+        /*if (coyoteTimer > 0)
+        {
+            coyoteTimer -= Time.deltaTime;
+            if (coyoteTimer <= 0)
+                canJump = false;
+        }*/
+
+        // Animation switching
+        if (!isGrounded)
+        {
+            animator.enabled = false;
+            sr.sprite = jumpSprite;
+        }
+        else
+        {
+            animator.enabled = true;
+        }
     }
 
     private void FixedUpdate()
     {
-        moveInput = playerInput.actions["Move"].ReadValue<Vector2>();
-        Vector2 Inputvector = new Vector2(moveInput.x,0);
-        Vector2 movement = Inputvector * speed;
-        rb.velocity = new Vector2 (movement.x,rb.velocityY);
-    }
-    private void Update()
-    {
-        if (rb.velocity.y < 0)
+        // Apply horizontal movement
+        Vector2 velocity = rb.velocity;
+        velocity.x = moveInput.x * runSpeed;
+        rb.velocity = velocity;
+
+        // Flip player direction
+        if (moveInput.x > 0.01f)
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        else if (moveInput.x < -0.01f)
+            transform.eulerAngles = new Vector3(0, 180, 0);
+
+        // Move with platform
+        if (currentPlatform != null)
         {
-            if (Physics2D.Raycast(transform.position, -Vector3.up, transform.localScale.y / 4, LayerMask.GetMask("Ground")))
-            {
-                canJump = true;
-                coyoteTimer = -1;
-            }
-            else
-            {
-                if(coyoteTimer <= 0)
-                coyoteTimer = coyoteTime; 
-            }
-        }
-        if(coyoteTimer > 0)
-        {
-            coyoteTimer -= Time.deltaTime;
-            if (coyoteTimer <= 0)
-            {
-                canJump = false;
-            }
+            Vector3 delta = currentPlatform.position - lastPlatformPos;
+            transform.position += delta;
+            lastPlatformPos = currentPlatform.position;
         }
     }
 
     public void Jump(InputAction.CallbackContext context)
     {
-        if (canJump && context.performed)
+        if (context.performed && canJump)
         {
-            rb.AddForceY(jumpStrenght);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             canJump = false;
+            coyoteTimer = -1;
+            currentPlatform = null;
         }
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("MovingPlatform"))
+        {
+            foreach (ContactPoint2D contact in collision.contacts)
+            {
+                if (contact.normal.y > 0.5f)
+                {
+                    currentPlatform = collision.transform;
+                    lastPlatformPos = currentPlatform.position;
+                    break;
+                }
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("MovingPlatform"))
+        {
+            if (currentPlatform == collision.transform)
+            {
+                currentPlatform = null;
+            }
+        }
+    }
 }
