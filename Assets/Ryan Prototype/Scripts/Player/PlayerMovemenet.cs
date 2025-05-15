aa2ef2490d7be288b7990a15509de831e335916d
KrijnSlot <krijnslot112233@gmail.com>
2025-05-15 16:13:54 +0200

    gravity Scene done(ish)

--- Assets/Ryan Prototype/Scripts/Player/PlayerMovemenet.cs
+++ Assets/Ryan Prototype/Scripts/Player/PlayerMovemenet.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using static UnityEngine.Rendering.DebugUI.Table;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(PlayerInput))]
@@ -10,11 +9,11 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
     public float runSpeed = 6f;
-    public float speedIncrement = 1f; // New variable for speed increment
+    public float speedIncrement = 1f;
 
     [Header("Jumping")]
     public float jumpForce = 8f;
-    public float jumpIncrement = 1f; // New variable for jump force increment
+    public float jumpIncrement = 1f;
     public float coyoteTime = 0.2f;
     private float coyoteTimer = -1;
     public bool canJump = false;
@@ -49,39 +48,35 @@ public class PlayerMovement : MonoBehaviour
         powers = GetComponent<PlayerPowers>();
 
         playerInput.actions["Jump"].performed += Jump;
-
     }
 
     private void Update()
     {
-        // Adjust runSpeed and jumpForce based on the player's scale
+        // Dynamic stats
         runSpeed = 6f * transform.localScale.x * speedIncrement;
         jumpForce = 8f * transform.localScale.x * jumpIncrement;
 
+        // Movement input
         moveInput = playerInput.actions["Move"].ReadValue<Vector2>();
-        Vector2 Inputvector = new Vector2(moveInput.x, 0);
-        Vector2 movement = Inputvector * runSpeed;
-        rb.velocity = new Vector2(movement.x, rb.velocity.y);
+        rb.velocity = new Vector2(moveInput.x * runSpeed, rb.velocity.y);
         TurnCheck();
 
+        // Ground check for jumping/coyote
         if ((rb.velocity.y < 0 && rb.gravityScale > 0) || (rb.velocity.y > 0 && rb.gravityScale < 0))
-        {
             JumpCheck();
-        }
 
-
+        // Coyote timer
         if (coyoteTimer > 0)
         {
             coyoteTimer -= Time.deltaTime;
             if (coyoteTimer <= 0)
                 canJump = false;
         }
-
     }
 
     private void FixedUpdate()
     {
-        if (Physics2D.Raycast(transform.position, -Vector3.up, transform.localScale.y / 4, LayerMask.GetMask("Platform")))
+        if (currentPlatform != null)
         {
             Vector3 delta = currentPlatform.position - lastPlatformPos;
             transform.position += delta;
@@ -89,9 +84,18 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+
     void JumpCheck()
     {
-        if (Physics2D.Raycast(transform.position, -Vector3.up * rb.gravityScale, transform.localScale.y / 4, LayerMask.GetMask("Ground")))
+        // Combine ground and platform layers
+        LayerMask combinedLayer = groundLayer | LayerMask.GetMask("Platform");
+
+        Vector2 rayDir = Vector2.down * Mathf.Sign(rb.gravityScale); // handles flipped gravity
+        float rayLength = transform.localScale.y / 4f;
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, rayDir, rayLength, combinedLayer);
+
+        if (hit.collider != null)
         {
             canJump = true;
             coyoteTimer = -1;
@@ -104,13 +108,13 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+
     public void Jump(InputAction.CallbackContext context)
     {
         if (context.performed && canJump)
         {
-            if(!powers.flipped)
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-            else rb.velocity = new Vector2(rb.velocity.x, -jumpForce);
+            float direction = powers.flipped ? -1f : 1f;
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce * direction);
             canJump = false;
             coyoteTimer = -1;
             currentPlatform = null;
@@ -121,68 +125,49 @@ public class PlayerMovement : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("MovingPlatform"))
         {
-            Debug.Log("touching platform");
             foreach (ContactPoint2D contact in collision.contacts)
             {
-                if (contact.normal.y > 0.5f)
+                float normalY = contact.normal.y;
+
+                // If gravity is normal, look for contact from below (normal pointing up)
+                // If gravity is flipped, look for contact from above (normal pointing down)
+                if ((!powers.flipped && normalY > 0.5f) || (powers.flipped && normalY < -0.5f))
                 {
                     currentPlatform = collision.transform;
                     lastPlatformPos = currentPlatform.position;
+                    Debug.Log("Attached to moving platform");
                     break;
                 }
             }
         }
     }
 
+
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("MovingPlatform"))
+        if (collision.gameObject.CompareTag("MovingPlatform") && currentPlatform == collision.transform)
         {
-            if (currentPlatform == collision.transform)
-            {
-                currentPlatform = null;
-            }
+            currentPlatform = null;
         }
     }
 
     void TurnCheck()
     {
-        if (moveInput.x > 0)
-        {
-            Turn();
-        }
-        else if (moveInput.x < 0)
-        {
-            Turn();
-        }
+        if (moveInput.x > 0 && !facingRight) Turn();
+        else if (moveInput.x < 0 && facingRight) Turn();
     }
 
     void Turn()
     {
+        Vector3 rot;
+
         if (moveInput.x < 0)
-        {
-            print("PlayerTrans: " + gameObject.transform.eulerAngles);
-            Vector3 rot;
-            if (powers.flipped) rot = new Vector3(180f, 180f, 0);
-            else rot = new Vector3(0f, 180f, 0);
-            print("Rotation: " + rot);
-            transform.localEulerAngles = rot;
-            print("PlayerTrans2: " + transform.rotation.eulerAngles);
-            if (facingRight) camFollow.CallTurn();
-            facingRight = false;
-        }
-        else if (moveInput.x > 0)
-        {
-            print("PlayerTrans: " + gameObject.transform.eulerAngles);
-            Vector3 rot;
-            if (powers.flipped) rot = new Vector3(180f, 0f, 0);
-            else rot = new Vector3(0f, 0f, 0);
-            transform.localEulerAngles = rot;
-            print("PlayerTrans2: " + transform.rotation.eulerAngles);
-            if (!facingRight) camFollow.CallTurn();
-            facingRight = true;
-        }
+            rot = powers.flipped ? new Vector3(180f, 180f, 0) : new Vector3(0f, 180f, 0);
+        else
+            rot = powers.flipped ? new Vector3(180f, 0f, 0) : new Vector3(0f, 0f, 0);
 
+        transform.localEulerAngles = rot;
+        camFollow.CallTurn();
+        facingRight = moveInput.x > 0;
     }
-
 }
