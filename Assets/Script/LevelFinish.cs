af2d413afc55e41086e4bdb29a294e19c2dd554e
Bo <bohulst@duck.com>
2025-05-19 18:14:55 +0200

    mvp gravity level with finish and death

--- Assets/Script/LevelFinish.cs
+++ Assets/Script/LevelFinish.cs
@@ -5,13 +5,11 @@ namespace Script
     public class LevelFinish : MonoBehaviour
     {
         [SerializeField] private LevelEndUI levelEndUI;
-        private void OnTriggerEnter2D(Collider2D collider)
+        private void OnTriggerEnter2D(Collider2D collision)
         {
-            if (collider.CompareTag("Player1") || collider.CompareTag("Player2"))
-            {
-                Debug.Log("Level Finished");
-                ShowLevelEndUI();
-            }
+            if (!collision.CompareTag("Player1") && !collision.CompareTag("Player2")) return;
+            Debug.Log("Level Finished");
+            ShowLevelEndUI();
         }
         
         private void ShowLevelEndUI()
