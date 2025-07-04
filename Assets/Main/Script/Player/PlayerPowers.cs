30827ef5acd837228b8ffbad3bca77b3ee97846f
KrijnSlot <krijnslot112233@gmail.com>
2025-07-04 14:13:30 +0200

    idk man just go

--- Assets/Main/Script/Player/PlayerPowers.cs
+++ Assets/Main/Script/Player/PlayerPowers.cs
@@ -61,7 +61,7 @@ public class PlayerPowers : MonoBehaviour
             {
                 case Powers.gravityManip: flipped = true; canFlip = true; Flip(); break;
                 case Powers.sizeManip: sizaManipOn = true; currentSize = PlayerSizes.normal; break;
-                case Powers.astralProject: isProjecting = true; AstralProjection(); playerObj.transform.position = Die.GetLastCheckpoint().transform.position; break;
+                case Powers.astralProject: isProjecting = true; AstralProjection(); PlayerObj.transform.position = Die.GetLastCheckpoint().transform.position; break;
             }
         }
     }
