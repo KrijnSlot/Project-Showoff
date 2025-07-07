79ebd808a62f14f84c1ce645a1081b732c1c882c
R-Bleach <loljk366@gmail.com>
2025-07-07 13:33:10 +0200

    finish works now

--- Assets/Main/Script/GameManagers/LevelFinish.cs
+++ Assets/Main/Script/GameManagers/LevelFinish.cs
@@ -7,7 +7,7 @@ namespace Script
         [SerializeField] private FinishManager finishManager;
         private void OnTriggerEnter2D(Collider2D collision)
         {
-            if (!collision.CompareTag("Player1") && !collision.CompareTag("Player2")) return;
+            if (!collision.CompareTag("Player1") || !collision.CompareTag("Player2")) return;
 
             if (collision.CompareTag("Player1"))
             {
@@ -18,6 +18,20 @@ namespace Script
                 finishManager.player2Finished = true;
             }
         }
-   
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (!collision.CompareTag("Player1") || !collision.CompareTag("Player2")) return;
+
+            if (collision.CompareTag("Player1"))
+            {
+                finishManager.player1Finished = false;
+            }
+            else if (collision.CompareTag("Player2"))
+            {
+                finishManager.player2Finished = false;
+            }
+        }
+
     }
 }
